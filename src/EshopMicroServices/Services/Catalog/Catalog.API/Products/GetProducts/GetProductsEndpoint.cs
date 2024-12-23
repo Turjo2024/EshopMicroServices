cf8bf5f011d6cb78916be7c8236dc96fed4c0673
Turjo2024 <writetoturjo@gmail.com>
2024-12-23 23:04:12 +0600

    GetProducts

---
+++ src/EshopMicroServices/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Catalog.API.Products.GetProducts
+{
+    //public record GetProductsRequest();
+
+    public record GetProdcutsResponse(IEnumerable<Product> products);
+
+    public class GetProductsEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products", async (ISender sender) =>
+            {
+                var result = await sender.Send(new GetProductsQuery());
+
+                var response = result.Adapt<GetProdcutsResponse>();
+
+                return Results.Ok(response);
+            })
+            .WithName("GetProducts")
+            .Produces<GetProdcutsResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Products")
+            .WithDescription("Get Products");
+        }
+    }
+}
