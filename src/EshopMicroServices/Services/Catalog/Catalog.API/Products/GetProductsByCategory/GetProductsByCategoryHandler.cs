0947c6f3d6ec1c71bb382cfcf67e0c6d0fd94717
Turjo2024 <writetoturjo@gmail.com>
2024-12-27 20:27:30 +0600

    Update.Product

---
+++ src/EshopMicroServices/Services/Catalog/Catalog.API/Products/GetProductsByCategory/GetProductsByCategoryHandler.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Catalog.API.Products.GetProductsByCategory
+{
+    public record GetProductByCategoryQuery(string category) : IQuery<GetProductByCategoryResult>;
+
+    public record GetProductByCategoryResult(IEnumerable<Product> Products);
+
+    internal class GetProductsByCategoryHandler
+        (IDocumentSession session, ILogger<GetProductsByCategoryHandler> logger)
+        : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
+    {
+        public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductsByCategoryHandler.Handle called with {query}", query);
+
+            var products = await session.Query<Product>()
+                .Where(p => p.Category.Contains(query.category))
+                .ToListAsync();
+
+            return new GetProductByCategoryResult(products);
+        }
+    }
+}
